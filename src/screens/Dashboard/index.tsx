e756ed7671057f68f300dfa5eb6acf30bf0b3726
NettoVieira <joao.neto@allrisk.com.br>
2021-06-09 20:50:03 -0300

    adicionando header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+          
+            <Photo source={{uri:'https://avatars.githubusercontent.com/u/46529740?v=4' }}/>
+            <User>
+              <UserGreeting>Ola, </UserGreeting>
+              <UserName>João</UserName>
+            </User>
+
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
