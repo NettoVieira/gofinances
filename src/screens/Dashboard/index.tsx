6d4d7ab1e3e0cc25fa19eda537bbdba22478a756
NettoVieira <joao.fvneto@hotmail.com>
2021-09-27 07:10:38 -0300

    finalizando a dashboard 😊

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { 
   Container,
   Header,
@@ -7,23 +8,25 @@ import {
   Photo,
   User,
   UserGreeting,
-  UserName
+  UserName,
+  Icon,
+  Status
 } from './styles';
 
 export function Dashboard() {
   return (
     <Container>
+      <Status backgroundColor="#5636D3"></Status>
       <Header>
         <UserWrapper>
           <UserInfo>
-          
             <Photo source={{uri:'https://avatars.githubusercontent.com/u/46529740?v=4' }}/>
             <User>
               <UserGreeting>Ola, </UserGreeting>
               <UserName>João</UserName>
             </User>
-
           </UserInfo>
+          <Icon name="power"/>
         </UserWrapper>
       </Header>
     </Container>
